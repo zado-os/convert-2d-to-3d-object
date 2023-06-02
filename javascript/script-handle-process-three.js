a79938adda09de6d2bcbff6cdc6cd0a9b68658cc
Hussain Al-zadjali <73733247+zado-os@users.noreply.github.com>
2023-06-02 08:58:59 +0400

    Fix path js in html

--- javascript/script-handle-process-three.js
+++ javascript/script-handle-process-three.js
@@ -3,7 +3,12 @@ var scene, camera, renderer;
 function init() {
   scene = new THREE.Scene();
 
-  camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+  camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  );
   camera.position.z = 5;
 
   renderer = new THREE.WebGLRenderer();
@@ -12,39 +17,41 @@ function init() {
 }
 
 function createObject(texture) {
-    var geometry = new THREE.BoxGeometry(1, 1, 1);
-    var material = new THREE.MeshBasicMaterial({ map: texture });
-    var cube = new THREE.Mesh(geometry, material);
-    scene.add(cube);
-  }
-  function animate() {
-    requestAnimationFrame(animate);
-  
-    // Rotate the object
-    scene.children[0].rotation.x += 0.01;
-    scene.children[0].rotation.y += 0.01;
-  
-    renderer.render(scene, camera);
-  }
-  
-  function handleImageUpload(event) {
-    var file = event.target.files[0];
-    var reader = new FileReader();
-  
-    reader.onload = function (event) {
-      var image = new Image();
-      image.onload = function () {
-        var texture = new THREE.Texture(image);
-        texture.needsUpdate = true;
-  
-        createObject(texture);
-        animate();
-      };
-      image.src = event.target.result;
-    };
+  var geometry = new THREE.BoxGeometry(1, 1, 1);
+  var material = new THREE.MeshBasicMaterial({ map: texture });
+  var cube = new THREE.Mesh(geometry, material);
+  scene.add(cube);
+}
+function animate() {
+  requestAnimationFrame(animate);
 
-    reader.readAsDataURL(file);
+  // Rotate the object
+  scene.children[0].rotation.x += 0.01;
+  scene.children[0].rotation.y += 0.01;
+
+  renderer.render(scene, camera);
+}
+
+function handleImageUpload(event) {
+  var file = event.target.files[0];
+  var reader = new FileReader();
+
+  reader.onload = function (event) {
+    var image = new Image();
+    image.onload = function () {
+      var texture = new THREE.Texture(image);
+      texture.needsUpdate = true;
+
+      createObject(texture);
+      animate();
+    };
+    image.src = event.target.result;
+  };
+
+  reader.readAsDataURL(file);
 }
 
 init();
-document.getElementById('imageUpload').addEventListener('change', handleImageUpload, false);
+document
+  .getElementById("imageUpload")
+  .addEventListener("change", handleImageUpload, false);
