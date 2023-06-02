5c8c0a3efee44d3f88900f76b777dab27cf72e64
Hussain Al-zadjali <73733247+zado-os@users.noreply.github.com>
2023-06-02 08:52:45 +0400

    create function to var scene,camera,render

---
+++ javascript/script-handel-process-three.js
@@ -0,0 +1,13 @@
+var scene, camera, renderer;
+
+function init() {
+  scene = new THREE.Scene();
+
+  camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+  camera.position.z = 5;
+
+  renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
+}
+
