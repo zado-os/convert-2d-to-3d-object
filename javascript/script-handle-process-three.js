bceff6dd85c8c7da4d8b1e12c279e4c19884ac62
Hussain Al-zadjali <73733247+zado-os@users.noreply.github.com>
2023-06-02 14:04:00 +0400

    # size obj + eventListner mouse

--- javascript/script-handle-process-three.js
+++ javascript/script-handle-process-three.js
@@ -1,43 +1,45 @@
-var scene, camera, renderer;
+var scene, camera, renderer, cube;
+var mouseDown = false;
+var mouseX = 0, mouseY = 0;
 
-// function initializes the Three.js scene, camera, and renderer.
-//It creates a scene, sets up a perspective camera,
-//and adds a WebGL renderer to the HTML document.
 function init() {
   scene = new THREE.Scene();
 
-  camera = new THREE.PerspectiveCamera(
-    75,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    1000
-  );
+  camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
   camera.position.z = 5;
 
   renderer = new THREE.WebGLRenderer();
   renderer.setSize(window.innerWidth, window.innerHeight);
   document.body.appendChild(renderer.domElement);
+
+  document.addEventListener('mousemove', handleMouseMove, false);
+  document.addEventListener('mousedown', handleMouseDown, false);
+  document.addEventListener('mouseup', handleMouseUp, false);
 }
-// function creates a 3D object (a cube) using a provided texture.
-//It uses a BoxGeometry to define the cube's shape and applies a MeshBasicMaterial with the given texture to the cube.
-//Finally, it adds the cube to the scene.
+
 function createObject(texture) {
-  var geometry = new THREE.BoxGeometry(1, 1, 1);
+  var geometry = new THREE.BoxGeometry(2, 2, 2);
   var material = new THREE.MeshBasicMaterial({ map: texture });
-  var cube = new THREE.Mesh(geometry, material);
+  cube = new THREE.Mesh(geometry, material);
   scene.add(cube);
 }
-// function is a recursive animation loop
+
 function animate() {
   requestAnimationFrame(animate);
 
   // Rotate the object
-  scene.children[0].rotation.x += 0.01;
-  scene.children[0].rotation.y += 0.01;
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+
+  // Move the object based on mouse position
+  if (mouseDown) {
+    cube.position.x = (mouseX / window.innerWidth) * 2 - 1;
+    cube.position.y = -(mouseY / window.innerHeight) * 2 + 1;
+  }
 
   renderer.render(scene, camera);
 }
-// function is the event handler for the file upload input. It reads the selected image file using a FileReader
+
 function handleImageUpload(event) {
   var file = event.target.files[0];
   var reader = new FileReader();
@@ -56,8 +58,19 @@ function handleImageUpload(event) {
 
   reader.readAsDataURL(file);
 }
-// function is called initially to set up the scene, camera, and renderer.
+
+function handleMouseMove(event) {
+  mouseX = event.clientX;
+  mouseY = event.clientY;
+}
+
+function handleMouseDown() {
+  mouseDown = true;
+}
+
+function handleMouseUp() {
+  mouseDown = false;
+}
+
 init();
-document
-  .getElementById("imageUpload")
-  .addEventListener("change", handleImageUpload, false);
+document.getElementById('imageUpload').addEventListener('change', handleImageUpload, false);
